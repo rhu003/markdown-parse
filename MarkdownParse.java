2ee2347e61702aca5fce86f7c301f75850aba3db
rhu003 <rhu@ucsd.edu>
2022-02-15 14:57:26 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,6 +17,11 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+
+            System.out.println(currentIndex);
+            if (currentIndex == 105) {
+                break;
+            }
         }
         return toReturn;
     }
