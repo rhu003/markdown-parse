e872252d6c1e99474c19687c4ab5ced8c0fa5e4d
rhu003 <rhu@ucsd.edu>
2022-02-15 16:26:44 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,7 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openParen == -1){
+            if (openParen == -1 || closeParen == -1){
                 break;
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
